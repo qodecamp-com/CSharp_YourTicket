7937d7f538b7c90fd3f5427a4778fe3907decd0a
Andy V <andyv@qodecamp.com>
2023-01-05 20:26:17 -0500

    Variables

---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Variables;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //declare some variables
+        int age;
+        string name;
+        bool isValid;
+
+        //assign values to variables
+        age = 30;
+        name = "John";
+        isValid = true;
+
+        //print out the values stored in the variables
+        Console.WriteLine("age = {0}", age);
+        Console.WriteLine("name = {0}", name);
+        Console.WriteLine("isValid = {0}", isValid);
+
+        //you can also print multiple variables in a single print statement
+        Console.WriteLine("Your name is {0}, and your age is {1}", name, age);
+    }
+}

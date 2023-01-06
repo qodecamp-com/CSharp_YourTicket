a007257c8d4d76b51e2c56f49280f861eeac6375
Andy V <andyv@qodecamp.com>
2023-01-05 20:52:45 -0500

    Type Conversion
    This project covers implicit and explicit type conversion, as well as an example of data loss.

---
+++ Chapter02/TypeConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TypeConversion;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 10;
+        long y = x;
+
+        Console.WriteLine("Implicit Type Conversion in action...");
+        Console.WriteLine("x = {0}, and y={1}", x, y);
+
+        long a = 10;
+        //cannot use implicit conversion to cast a long to an int - this is not allowed
+        //int b = a;
+
+        //we have to use explicit casting/type conversion
+        int b = (int)a;  // this works
+        Console.WriteLine("Explicit Type Conversion in action...");
+        Console.WriteLine("a = {0}, and b={1}", a, b);
+
+        //finally here's an example that produces an unexpected result
+        long c = 20000000000;
+        float d = c;
+        Console.WriteLine(d); //d and e will have different values due to data loss
+        int e = (int)d;
+        Console.WriteLine(e);
+    }
+}
